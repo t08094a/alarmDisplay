dd1a0d09096aa6b8bd547bbdcf895378c034bd28
t08094a <jochen.faehnlein@ff-ipsheim.de>
2018-01-08 21:59:28 +0100

    navigation läuft

--- alarmDisplayApp/src/app/app.module.ts
+++ alarmDisplayApp/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
 
 import { AppComponent } from './app.component';
 import { AlarmInfoComponent } from './alarm-info/alarm-info.component';
 import { HydrantplanComponent } from './hydrantplan/hydrantplan.component';
 import { CommonInfoComponent } from './common-info/common-info.component';
 import { NavigationComponent } from './navigation/navigation.component';
-
 
 @NgModule({
   declarations: [
@@ -18,7 +18,11 @@ import { NavigationComponent } from './navigation/navigation.component';
     NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCfKS11Jj_0N9kNDHMtNSNGxjGs6pEQxz0'
+    }),
+    AgmDirectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- alarmDisplayApp/src/app/navigation/navigation.component.ts
+++ alarmDisplayApp/src/app/navigation/navigation.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, NgModule, OnInit } from '@angular/core';
+import { GoogleMapsAPIWrapper,  LatLngLiteral,   MouseEvent,   MapsAPILoader} from '@agm/core';
+import { } from 'googlemaps';
+import { GMapsService } from './gmaps-service.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  providers: [ GMapsService ]
 })
+
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  public title = 'Einsatz Navigation';
+  public zoom = 15;
+  public dir = undefined;
+
+  private origin = { lat: 49.526558948981595, lng: 10.483931601047516};
+  private address = 'Am Schießwasen 2, 91438 Bad Windsheim';
+
+  constructor(private mapsAPILoader: MapsAPILoader,
+    private gMapsService: GMapsService,
+    private __zone: NgZone) { }
 
   ngOnInit() {
+    // load places autocomplete
+    this.mapsAPILoader.load().then(() => {
+        this.renderDirections();
+    });
   }
 
+  ngOnChange() {
+      this.renderDirections();
+  }
+
+  renderDirections() {
+    this.gMapsService.getLatLng(this.address)
+      .subscribe(
+        result => {
+            this.__zone.run(() => {
+                let destination_lat = result.lat();
+                let destination_lng = result.lng();
+
+                console.log('LatLng for ', this.address, ': ', destination_lat, ', ', destination_lng);
+
+                this.dir = {
+                    origin: this.origin,
+                    destination: new google.maps.LatLng(destination_lat, destination_lng),
+                    travelMode: google.maps.TravelMode.DRIVING
+                };
+
+                // this.gMapsService.panBy(this.target_lat, this.target_lng);
+                this.gMapsService.setZoom(15);
+            })
+        },
+        error => console.log(error),
+        () => console.log('Geocoding completed!')
+      );
+  }
+
+  onMapClicked(event: MouseEvent) {
+      console.log(event.coords);
+  }
 }
