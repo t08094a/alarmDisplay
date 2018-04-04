cd999be933101c0685df77789a5680cb0881b874
t08094a <jochen.faehnlein@ff-ipsheim.de>
2018-04-04 11:57:44 +0200

    Title setzen

--- alarmDisplayApp/src/app/app.component.ts
+++ alarmDisplayApp/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = environment.applicationTitle;
-  appVersion = environment.VERSION;
+    title = environment.applicationTitle;
+    appVersion = environment.VERSION;
+
+    public constructor(private titleService: Title) {
+        this.titleService.setTitle(environment.applicationTitle);
+    }
 }
