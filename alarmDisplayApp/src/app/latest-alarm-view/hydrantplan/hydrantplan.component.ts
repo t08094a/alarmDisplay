43f2c959da472c03d69c6e3ed3d155d2a20e0c7a
t08094a <jochen.faehnlein@ff-ipsheim.de>
2018-04-04 12:00:40 +0200

    Code Formatierung  Hydrantenplan

--- alarmDisplayApp/src/app/latest-alarm-view/hydrantplan/hydrantplan.component.ts
+++ alarmDisplayApp/src/app/latest-alarm-view/hydrantplan/hydrantplan.component.ts
@@ -1,92 +1,118 @@
 import { Component, OnInit } from '@angular/core';
-// tslint:disable-next-line:max-line-length
-import { icon, latLng, Map, marker, point, polyline, tileLayer, TileLayer, LeafletEvent, LeafletMouseEvent, Popup, Layer, Marker, LayerGroup, circle, layerGroup, MapOptions } from 'leaflet';
+import {
+    icon,
+    latLng,
+    Map,
+    marker,
+    point,
+    polyline,
+    tileLayer,
+    TileLayer,
+    LeafletEvent,
+    LeafletMouseEvent,
+    Popup,
+    Layer,
+    Marker,
+    LayerGroup,
+    circle,
+    layerGroup,
+    MapOptions
+} from 'leaflet';
 import { OverpassService } from './services/overpass.service';
 import { MarkerCreatorService } from './services/marker-creator.service';
 
 @Component({
-  selector: 'app-hydrantplan',
-  templateUrl: './hydrantplan.component.html',
-  styleUrls: ['./hydrantplan.component.css']
+    selector: 'app-hydrantplan',
+    templateUrl: './hydrantplan.component.html',
+    styleUrls: ['./hydrantplan.component.css']
 })
 export class HydrantplanComponent implements OnInit {
+    public title = 'Hydrantenplan';
 
-  public title = 'Hydrantenplan';
+    private zoomLevel = 17;
 
-  private zoomLevel = 17;
+    private googleMaps: TileLayer = tileLayer(
+        'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        {
+            maxZoom: 20,
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+            detectRetina: true
+        }
+    );
 
-  private googleMaps: TileLayer = tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    detectRetina: true
-  });
+    private openStreetMap: TileLayer = tileLayer(
+        'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+        {
+            attribution: '',
+            detectRetina: true,
+            maxZoom: 18
+        }
+    );
 
-  private openStreetMap: TileLayer = tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-    attribution: '',
-    detectRetina: true,
-    maxZoom: 18
-  });
-
-  private ziel: Marker = marker([49.527352, 10.487624], {
-    icon: icon({
-      iconSize: [25, 41],
-      iconAnchor: [13, 41],
-      iconUrl: 'leaflet/marker-icon.png',
-      shadowUrl: 'leaflet/marker-shadow.png'
-    })
-  });
-
-  private radiusCircle = circle(this.ziel.getLatLng(), {
-      radius: 100
+    private ziel: Marker = marker([49.527352, 10.487624], {
+        icon: icon({
+            iconSize: [25, 41],
+            iconAnchor: [13, 41],
+            iconUrl: 'leaflet/marker-icon.png',
+            shadowUrl: 'leaflet/marker-shadow.png'
+        })
     });
 
-  layersControl = {
-    baseLayers: {
-      'Google Maps': this.googleMaps,
-      'OpenStreetMap': this.openStreetMap
-    },
-    overlays: {
-      'Ziel': this.ziel,
-      'Einsatzradius': this.radiusCircle
-    }
-  };
+    private radiusCircle = circle(this.ziel.getLatLng(), {
+        radius: 100
+    });
 
-  options: MapOptions = {
-    layers: [this.openStreetMap, this.ziel, this.radiusCircle],
-    zoom: 15,
-    center: latLng([49.526558948981595, 10.483931601047516]),
-    zoomControl: null
-  };
+    layersControl = {
+        baseLayers: {
+            'Google Maps': this.googleMaps,
+            OpenStreetMap: this.openStreetMap
+        },
+        overlays: {
+            Ziel: this.ziel,
+            Einsatzradius: this.radiusCircle
+        }
+    };
 
-  private map: Map;
+    options: MapOptions = {
+        layers: [this.openStreetMap, this.ziel, this.radiusCircle],
+        zoom: 15,
+        center: latLng([49.526558948981595, 10.483931601047516]),
+        zoomControl: null
+    };
 
-  constructor(private overpassService: OverpassService,
-              private markerCreator: MarkerCreatorService) {
-  }
+    private map: Map;
 
-  ngOnInit() {}
+    constructor(
+        private overpassService: OverpassService,
+        private markerCreator: MarkerCreatorService
+    ) {}
+
+    ngOnInit() {}
 
     public onMapReady(map: Map): void {
-    this.map = map;
+        this.map = map;
 
-    map.setView(this.ziel.getLatLng(), this.zoomLevel);
+        map.setView(this.ziel.getLatLng(), this.zoomLevel);
 
-    this.radiusCircle.setLatLng(this.ziel.getLatLng());
+        this.radiusCircle.setLatLng(this.ziel.getLatLng());
 
-    this.markerCreator.mapToHydrantMarker(this.overpassService.getHydrantMarkers(map.getBounds()))
-        .then(m => {
-          const group = new LayerGroup(m);
-          map.addLayer(group);
-          this.layersControl.overlays['Hydranten'] = group;
-        })
-        .catch(error => console.log(error));
+        this.markerCreator
+            .mapToHydrantMarker(
+                this.overpassService.getHydrantMarkers(map.getBounds())
+            )
+            .then(m => {
+                const group = new LayerGroup(m);
+                map.addLayer(group);
+                this.layersControl.overlays['Hydranten'] = group;
+            })
+            .catch(error => console.log(error));
 
-    if (!map.hasEventListeners('click')) {
-      map.addEventListener('click', this.onClick);
+        if (!map.hasEventListeners('click')) {
+            map.addEventListener('click', this.onClick);
+        }
     }
-  }
 
-  public onClick(event: LeafletMouseEvent ): void {
-    console.log(event.latlng);
-  }
+    public onClick(event: LeafletMouseEvent): void {
+        console.log(event.latlng);
+    }
 }
